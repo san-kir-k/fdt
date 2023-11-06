828061a9277f41352e4652b760147e560b93aeca
san-kir-k <dfomka@list.ru>
2023-11-06 22:17:07 +0300

    Improve bench, add memset example

--- lib/include/utility.h
+++ lib/include/utility.h
@@ -2,18 +2,30 @@
 
 #include <chrono>
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <numeric>
 
-#define BENCHMARK(msg, bytes, function, args...)                                                                \
-    do                                                                                                          \
-    {                                                                                                           \
-        using namespace std::chrono_literals;                                                                   \
-        using duration = std::chrono::duration<double>;                                                         \
-        constexpr auto GB = 1'000'000'000l;                                                                     \
-        auto start_time = std::chrono::high_resolution_clock::now();                                            \
-        function(args);                                                                                         \
-        auto end_time = std::chrono::high_resolution_clock::now();                                              \
-        duration time = end_time - start_time;                                                                  \
-        std::cerr << msg                                                                                        \
-                  << static_cast<double>(bytes) / (GB * time.count()) << " [GB/sec]"                            \
-                  << std::endl;                                                                                 \
+#define BENCHMARK(msg, bytes, function, args...)                                                                    \
+    do                                                                                                              \
+    {                                                                                                               \
+        using namespace std::chrono_literals;                                                                       \
+        using duration = std::chrono::duration<double>;                                                             \
+        constexpr auto GB = 1'000'000'000l;                                                                         \
+                                                                                                                    \
+        int tries = 35;                                                                                             \
+        std::vector<double> measures;                                                                               \
+        while (tries--)                                                                                             \
+        {                                                                                                           \
+            auto start_time = std::chrono::high_resolution_clock::now();                                            \
+            function(args);                                                                                         \
+            auto end_time = std::chrono::high_resolution_clock::now();                                              \
+            duration time = end_time - start_time;                                                                  \
+            measures.push_back(time.count());                                                                       \
+        }                                                                                                           \
+        std::sort(measures.begin(), measures.end());                                                                \
+        auto avgtime = std::accumulate(measures.begin() + 5, measures.end() - 5, 0.0) / static_cast<double>(25);    \
+        std::cerr << msg                                                                                            \
+                  << static_cast<double>(bytes) / (GB * avgtime) << " [GB/sec]"                                     \
+                  << std::endl;                                                                                     \
     } while (0)
